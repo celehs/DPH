8c9d5ba3eae159e349d3ec6c8d1ebe3d68f6a3b1
Ming Yang <hustyangming@gmail.com>
2020-12-03 02:11:30 +0000

    extract sub-function adph2_fit() from adph2()

--- R/adph2.R
+++ R/adph2.R
@@ -30,6 +30,38 @@ adph2_loglik <- function(beta, lambda0, lambda0_s, t0, d0, X, w = NULL) {
   return(sum(loglik))
 }
 
+adph2_fit <- function(time, status, X, w, start, method = "BFGS") {
+  nt <- max(time)  
+  np <- ncol(X) 
+  obj <- stats::optim(
+    par = start,
+    fn = function(par) {
+      - adph2_loglik(
+        beta = par[1:np],
+        lambda0 = stats::plogis(par[np + 1:nt]),
+        lambda0_s = stats::plogis(par[np + nt + 1:nt]),
+        t0 = c(time),
+        d0 = c(status), 
+        X = X,
+        w = w
+      )
+    },
+    method = method
+  )     
+  loglik <- -obj$value
+  beta <- obj$par[1:np]
+  lambda0 <- stats::plogis(obj$par[np + 1:nt]) 
+  lambda0_s <- stats::plogis(obj$par[np + nt + 1:nt])
+  AUC <- auc(score = c(X %*% beta), X = X, beta = beta, lambda0 = lambda0)
+  names(AUC) <- names(lambda0)   
+  return(list(
+    loglik = loglik,
+    beta = beta, 
+    lambda0 = lambda0,
+    lambda0_s = lambda0_s,
+    AUC = AUC))
+}
+
 #' Adjusted Discrete Proportional Hazards (ADPH) Model
 #' @param time a vector of discrete time (e.g. 1, 2, 3, ...)
 #' @param status a vector of event status (1 = observed, 0 = censored)
@@ -38,9 +70,10 @@ adph2_loglik <- function(beta, lambda0, lambda0_s, t0, d0, X, w = NULL) {
 #' @param lambda0_s ...
 #' @param n_ptb number of perturbations
 #' @param seed random number generation seed
+#' @param show_progress TRUE or FALSE (default)
 #' @export
 adph2 <- function(time, status, pred, lambda0 = NULL, lambda0_s = NULL, 
-                  n_ptb = 100, seed = 1) {
+                  n_ptb = 200, seed = 1, show_progress = FALSE) {
   n <- length(time)
   nt <- max(time)
   X <- as.matrix(pred)  
@@ -49,63 +82,41 @@ adph2 <- function(time, status, pred, lambda0 = NULL, lambda0_s = NULL,
   if (is.null(lambda0)) lambda0 <- fit0$lambda0
   if (is.null(lambda0_s)) lambda0_s <- lambda0
   start <- c(fit0$beta, stats::qlogis(lambda0), stats::qlogis(lambda0_s)) 
-  fit <- stats::optim(
-    par = start,
-    fn = function(par) {
-      (-1) * adph2_loglik(
-        beta = par[1:np],
-        lambda0 = stats::plogis(par[np + 1:nt]),
-        lambda0_s = stats::plogis(par[np + nt + 1:nt]),
-        t0 = c(time),
-        d0 = c(status), 
-        X = X
-      )
-    },
-    method = "BFGS"
-  )     
-  beta <- fit$par[1:np]
-  lambda0 <- stats::plogis(fit$par[np + 1:nt]) 
-  lambda0_s <- stats::plogis(fit$par[np + nt + 1:nt])
-  loglik <- -fit$value
-  AUC <- auc(score = c(X %*% beta), X = X, beta = beta, lambda0 = lambda0)
-  names(AUC) <- names(lambda0) 
-  beta_ptb <- data.frame(matrix(NA, n_ptb, np))
-  lambda0_ptb <- data.frame(matrix(NA, n_ptb, nt))
-  lambda0_s_ptb <- data.frame(matrix(NA, n_ptb, nt))
-  colnames(beta_ptb) <- names(beta)
-  colnames(lambda0_ptb) <- names(lambda0)
-  colnames(lambda0_s_ptb) <- names(lambda0_s)
+  fit <- adph2_fit(
+    time = time,
+    status = status,
+    X = X,
+    w = rep(1, n),
+    start = start
+  )
+  ptb <- NULL
+  ptb$beta <- data.frame(matrix(NA, n_ptb, np))
+  ptb$lambda0 <- data.frame(matrix(NA, n_ptb, nt))
+  ptb$lambda0_s <- data.frame(matrix(NA, n_ptb, nt))
+  ptb$AUC <- data.frame(matrix(NA, n_ptb, nt))
+  colnames(ptb$beta) <- names(fit$beta)
+  colnames(ptb$lambda0) <- names(fit$lambda0)
+  colnames(ptb$lambda0_s) <- names(fit$lambda0_s)
+  colnames(ptb$AUC) <- names(fit$AUC)
   set.seed(seed)
+  start <- c(fit$beta, fit$lambda0, fit$lambda0_s) 
   for (i in 1:n_ptb) {
-    w <- stats::rexp(n)
-    start <- c(beta, stats::qlogis(lambda0), stats::qlogis(lambda0_s)) 
-    fit <- stats::optim(
-      par = start,
-      fn = function(par) {
-        (-1) * adph2_loglik(
-          beta = par[1:np],
-          lambda0 = stats::plogis(par[np + 1:nt]),
-          lambda0_s = stats::plogis(par[np + nt + 1:nt]),
-          t0 = c(time),
-          d0 = c(status), 
-          X = X,
-          w = w
-        )
-      },
-      method = "BFGS"
-    )
-    beta_ptb[i, ] <- fit$par[1:np]
-    lambda0_ptb[i, ] <- stats::plogis(fit$par[np + 1:nt]) 
-    lambda0_s_ptb[i, ] <- stats::plogis(fit$par[np + nt + 1:nt])    
+    if (show_progress) print(paste0("perturbation: ", i, "/", n_ptb))
+    fit_ptb <- adph2_fit(
+      time = time,
+      status = status,
+      X = X,
+      w = stats::rexp(n),
+      start = start
+    ) 
+    ptb$beta[i, ] <- fit_ptb$beta
+    ptb$lambda0[i, ] <- fit_ptb$lambda0
+    ptb$lambda0_s[i, ] <- fit_ptb$lambda0_s   
+    ptb$AUC[i, ] <- fit_ptb$AUC
   }
-  return(list(
-    beta = beta, 
-    lambda0 = lambda0, 
-    lambda0_s = lambda0_s, 
-    loglik = loglik, 
-    AUC = AUC,
-    beta_ptb = tibble::as_tibble(beta_ptb),
-    lambda0_ptb = tibble::as_tibble(lambda0_ptb),
-    lambda0_s_ptb = tibble::as_tibble(lambda0_s_ptb)
-  ))  
+  ptb$beta <- tibble::as_tibble(ptb$beta)
+  ptb$lambda0 <- tibble::as_tibble(ptb$lambda0)
+  ptb$lambda0_s <- tibble::as_tibble(ptb$lambda0_s)
+  ptb$AUC <- tibble::as_tibble(ptb$AUC)
+  return(list(fit = fit, ptb = ptb))  
 }
